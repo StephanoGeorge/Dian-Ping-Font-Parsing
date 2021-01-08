91803dc902a4294ce43bec098c5c2cf760d98f8d
Stephano George <huohuo5230@gmail.com>
2021-01-08 22:11:29 +0800

    remove SDK description

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Dian Ping Font Parsing SDK (大众点评字体解析SDK)
+# Dian Ping Font Parsing (大众点评字体解析)
 
 # Installation
 
 ```shell
-pip install --upgrade git+https://github.com/StephanoGeorge/Dian-Ping-Font-Parsing-SDK.git
+pip install --upgrade git+https://github.com/StephanoGeorge/Dian-Ping-Font-Parsing.git
 ```
 
 # Usage
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name='Dian-Ping-Font-Parsing-SDK',
+    name='Dian-Ping-Font-Parsing',
     version='0.0.1',
     author='Stephano George',
     author_email='huohuo5230@gmail.com',
-    description='Dian Ping Font Parsing SDK',
-    url='https://github.com/StephanoGeorge/Dian-Ping-Font-Parsing-SDK',
+    description='Dian Ping Font Parsing',
+    url='https://github.com/StephanoGeorge/Dian-Ping-Font-Parsing',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
